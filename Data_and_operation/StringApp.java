f31983f238d0792e24868b4974e718791f16a138
unknown <dlckdals1004@naver.com>
2022-12-22 22:07:19 +0900

    Java Programming

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,19 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); // String
+		System.out.println('H'); //Character
+		System.out.println("H");
+
+		System.out.println("Hello"
+				+ "World"); // 띄어쓰기 출력은 안된다.
+		
+		// new line
+		System.out.println("Hello \nWorld");
+		
+		// escape
+		System.out.println("Hello \"World\"");
+	}
+
+}
